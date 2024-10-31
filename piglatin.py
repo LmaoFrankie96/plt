92fa04dacf29b17b6ffe4b8cee219a26a9845d37
Muhammad Ramish <mramish9696@gmail.com>
2024-10-31 17:12:33 +0200

    User Story 1: Test case passed for hello world

--- piglatin.py
+++ piglatin.py
@@ -1,10 +1,11 @@
 class PigLatin:
 
     def __init__(self, phrase: str):
+        self.phrase=phrase
         pass
 
     def get_phrase(self) -> str:
-        pass
+        return self.phrase
 
     def translate(self) -> str:
         pass
--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -2,8 +2,10 @@ import unittest
 from piglatin import PigLatin
 from error import PigLatinError
 
-
+#User story 1
 class TestPigLatin(unittest.TestCase):
 
-    def test_something(self):
+    def test_value_hello_world_returns_hello_world(self):
+        piglatin=PigLatin("hello world")
+        self.assertEqual("hello world",piglatin.get_phrase())
         pass
