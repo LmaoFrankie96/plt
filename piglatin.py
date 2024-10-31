9518fc667973aeff721d28ce33d93f0bf2545741
Muhammad Ramish <mramish9696@gmail.com>
2024-10-31 17:36:22 +0200

    User Story 2: no change, just some confusion which was resolved by instructor

--- piglatin.py
+++ piglatin.py
@@ -2,7 +2,7 @@ class PigLatin:
 
     def __init__(self, phrase: str):
         self.phrase=phrase
-        pass
+
 
     def get_phrase(self) -> str:
         return self.phrase
--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -4,11 +4,13 @@ from error import PigLatinError
 
 
 class TestPigLatin(unittest.TestCase):
+
     # User story 1
     def test_value_hello_world_returns_hello_world(self):
         piglatin=PigLatin("hello world")
         self.assertEqual("hello world",piglatin.get_phrase())
+    # User Story 2
     def test_value_emptyString_returns_nil(self):
-        piglatin=PigLatin("")
+        piglatin = PigLatin("")
         self.assertEqual("nil",piglatin.translate())
 
