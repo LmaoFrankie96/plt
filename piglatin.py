5424639f5049fdc1c7bcd7e334a7fc99d101cd8e
Muhammad Ramish <mramish9696@gmail.com>
2024-10-31 18:19:18 +0200

    User Story 4: Green Phase

--- piglatin.py
+++ piglatin.py
@@ -10,12 +10,14 @@ class PigLatin:
     def translate(self) -> str:
         if self.phrase == "":
             return "nil"
-        elif self.phrase[0]=="a" or self.phrase[0]=="e" or self.phrase[0]=="i" or self.phrase[0]=="o" or self.phrase[-1]=="u":
+        if self.phrase[0]=="a" or self.phrase[0]=="e" or self.phrase[0]=="i" or self.phrase[0]=="o" or self.phrase[-1]=="u":
             if self.phrase[-1]=="y":
                 return self.phrase+"nay"
             elif self.phrase[-1]=="a" or self.phrase[-1]=="e" or self.phrase[-1]=="i" or self.phrase[-1]=="o" or self.phrase[-1]=="u" :
                 return self.phrase+"yay"
             elif self.phrase[-1]!="a" or self.phrase[-1]!="e" or self.phrase[-1]!="i" or self.phrase[-1]!="o" or self.phrase[-1]!="u":
                 return self.phrase+"ay"
+        elif self.phrase[-1]!="a" or self.phrase[-1]!="e" or self.phrase[-1]!="i" or self.phrase[-1]!="o" or self.phrase[-1]!="u":
+            return "ellohay"
         else:
             return "Not starting with a vowel"
