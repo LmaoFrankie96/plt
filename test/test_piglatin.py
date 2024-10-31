76d9182ce048426ea0d0961eed2ded8c77dbf42d
Muhammad Ramish <mramish9696@gmail.com>
2024-10-31 17:17:18 +0200

    User Story 2: Red phase

--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -8,4 +8,7 @@ class TestPigLatin(unittest.TestCase):
     def test_value_hello_world_returns_hello_world(self):
         piglatin=PigLatin("hello world")
         self.assertEqual("hello world",piglatin.get_phrase())
+    def test_value_emptyString_returns_nil(self):
+        piglatin=PigLatin("")
+        self.assertEqual("nil",piglatin.translate())
 
