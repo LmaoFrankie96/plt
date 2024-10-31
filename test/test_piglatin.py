ae840c1d5e2a7f9962a1256c4e85726c651c2e62
Muhammad Ramish <mramish9696@gmail.com>
2024-10-31 18:03:55 +0200

    User Story 3: Test 3 Red Phase

--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -4,7 +4,6 @@ from error import PigLatinError
 
 
 class TestPigLatin(unittest.TestCase):
-
     # User story 1
     def test_value_hello_world_returns_hello_world(self):
         piglatin=PigLatin("hello world")
@@ -13,12 +12,13 @@ class TestPigLatin(unittest.TestCase):
     def test_value_emptyString_returns_nil(self):
         piglatin = PigLatin("")
         self.assertEqual("nil",piglatin.translate())
-
     #User Story 3
     def test_value_startsWithAVowel_and_wordEndsWithY_returns_wordAppendedWithNay(self):
         piglatin=PigLatin("any")
         self.assertEqual("anynay",piglatin.translate())
-
     def test_value_startsWithAVowel_and_wordEndsWithVowel_returns_wordAppendedWithYay(self):
         piglatin = PigLatin("apple")
         self.assertEqual("appleyay",piglatin.translate())
+    def test_value_startsWithAVowel_and_wordEndsWithConsonantThatIsNotY_returns_wordAppendedWithAy(self):
+        piglatin = PigLatin("ask")
+        self.assertEqual("askay",piglatin.translate())
