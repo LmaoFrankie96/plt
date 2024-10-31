fd99d97b6efa2252981fabf6b675675788e65c71
Muhammad Ramish <mramish9696@gmail.com>
2024-10-31 18:00:00 +0200

    User Story 3: Refactoring more for Test 1 and 2

--- piglatin.py
+++ piglatin.py
@@ -8,9 +8,12 @@ class PigLatin:
         return self.phrase
 
     def translate(self) -> str:
-        if self.phrase=="":
+        if self.phrase == "":
             return "nil"
-        elif self.phrase[-1]=="y":
-            return self.phrase+"nay"
-        elif self.phrase[-1]=="a" or self.phrase[-1]=="e" or self.phrase[-1]=="i" or self.phrase[-1]=="o" or self.phrase[-1]=="u" :
-            return self.phrase+"yay"
+        elif self.phrase[0]=="a" or self.phrase[0]=="e" or self.phrase[0]=="i" or self.phrase[0]=="o" or self.phrase[-1]=="u":
+            if self.phrase[-1]=="y":
+                return self.phrase+"nay"
+            elif self.phrase[-1]=="a" or self.phrase[-1]=="e" or self.phrase[-1]=="i" or self.phrase[-1]=="o" or self.phrase[-1]=="u" :
+                return self.phrase+"yay"
+        else:
+            return "Not starting with a vowel"
