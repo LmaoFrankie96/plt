b1570d38cdad0443d115a2198f6585b813a1d0e1
Muhammad Ramish <mramish9696@gmail.com>
2024-10-31 17:53:03 +0200

    User Story 3: Refactoring for Test 1 and 2

--- piglatin.py
+++ piglatin.py
@@ -10,7 +10,7 @@ class PigLatin:
     def translate(self) -> str:
         if self.phrase=="":
             return "nil"
-        elif self.phrase=="any":
-            return "anynay"
-        elif self.phrase=="apple":
-            return "appleyay"
+        elif self.phrase[-1]=="y":
+            return self.phrase+"nay"
+        elif self.phrase[-1]=="a" or self.phrase[-1]=="e" or self.phrase[-1]=="i" or self.phrase[-1]=="o" or self.phrase[-1]=="u" :
+            return self.phrase+"yay"
